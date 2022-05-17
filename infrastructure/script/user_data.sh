5ffdb2d189bb5ecccba9e1290544ce77afc21170
weiaws <weiaws@amazon.com>
2022-05-17 05:23:21 +0000

    add the git clone command to user data

--- infrastructure/script/user_data.sh
+++ infrastructure/script/user_data.sh
@@ -8,5 +8,6 @@ cd /home/ec2-user
 wget https://archive.apache.org/dist/kafka/3.0.1/kafka-3.0.1-src.tgz
 tar -xzf kafka-3.0.1-src.tgz
 rm -f kafka-3.0.1-src.tgz
+git clone --depth=1 git@github.com:wei-zhong90/deltalake-aws-poc-demo.git
 
 python3 -m pip install virtualenv
